15c7f3578cd9ebf1d6dfc504a94d8914f2e639f5
John Gicharu <gicharucode@gmail.com>
2024-09-15 08:07:54 +0300

    feat: first commit - binary_search_naive

---
+++ README.md
@@ -0,0 +1,7 @@
+# Data Structures and Algorithms Problems/Exploration in Golang
+
+In this repo, I am going to solved various Data Structures and Algorithms Problems in Golang.
+
+I'm also going to be exploring data structures here and try to implement my own solutions. I'll be starting with a naive solution first, then I can move on to an improved version of the solution.
+
+The naive solution in this case will be based on my own understanding of the Data Structure or the Algorithm (given my experience in JS/TS). Once I am done, I'll be doing some research to see how the solution can be improved or how experts in Golang tend to approach the problem.
---
+++ binary_search/binary_search_naive.go
@@ -0,0 +1,33 @@
+package binary_search_naive
+
+func binary_search(arr []int, find_item, low, high int) int {
+	// Assume the array is already sorted
+	mid := (high + low) / 2
+
+	if low <= high {
+		if arr[mid] == find_item {
+			return mid
+		} else if find_item < arr[mid] {
+			// Lesser than the mid, so value exists to the left of the middle point
+			return binary_search(arr, find_item, mid-1, high)
+		} else if find_item > arr[mid] {
+			// Item must be larger than the mid, so need to find it from the right side of the arr
+			return binary_search(arr, find_item, mid+1, high)
+		}
+	}
+
+	return -1 // Item is not in array
+}
+
+func main() {
+	// Array/Slice must be sorted
+	// Sorting the array should be simple though - with the slices package
+	// slices.Sort()
+	arr := []int{1, 2, 3, 4, 5}
+
+	high := len(arr) - 1
+	low := 0
+
+	// You can log this here to be sure it works as expected
+	binary_search(arr, 4, low, high)
+}
